d282091f2fa515b6cf788e78ec80d3e89d91a063
Thiago Mendes <thiagomendes_95@hotmail.com>
2021-07-11 01:04:47 -0300

    Implements Insert Vector Ordered

---
+++ datastructure-and-algorithms/src/main/java/br/com/dev/thiagomds/datastructure/Vectors/ordered/VectorOrderedApplicationMain.java
@@ -0,0 +1,42 @@
+package br.com.dev.thiagomds.datastructure.Vectors.ordered;
+
+import br.com.dev.thiagomds.datastructure.Vectors.unordered.VectorUnordered;
+
+public class VectorOrderedApplicationMain {
+
+    public static void main(String[] args) {
+        VectorOrdered vectorOrdered = new VectorOrdered();
+        vectorOrdered.createVector(10);
+        vectorOrdered.printVector();
+
+        System.out.println("\n");
+        vectorOrdered.insertVector(6);
+        vectorOrdered.printVector();
+
+        System.out.println("\n");
+        vectorOrdered.insertVector(4);
+        vectorOrdered.printVector();
+
+        System.out.println("\n");
+        vectorOrdered.insertVector(10);
+        vectorOrdered.printVector();
+
+        System.out.println("\n");
+        vectorOrdered.insertVector(8);
+        vectorOrdered.printVector();
+
+        System.out.println("\n");
+        vectorOrdered.insertVector(3);
+        vectorOrdered.printVector();
+
+        System.out.println("\n");
+        vectorOrdered.insertVector(15);
+        vectorOrdered.printVector();
+
+        System.out.println("\n");
+        vectorOrdered.insertVector(13);
+        vectorOrdered.printVector();
+
+
+    }
+}
